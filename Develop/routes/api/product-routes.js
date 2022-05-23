94a56709e24bfb3ebc289cc76c10dc9b089c35d0
Lucca Araujo <lucca.ara7@gmail.com>
2022-05-22 18:33:54 -0700

    added crud for the product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,28 +5,85 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+            model: Category,
+            attributes: ['category_name']
+        },
+        {
+            model: Tag,
+            attributes: ['tag_name']
+        }
+    ]
+  }).then(dbProduct => {
+    if (!dbProduct) {
+       // if page not found
+        res.status(404).json({ message: 'Unable to find the products' });
+        return;
+    }
+    res.json(dbTag);
+  })
+  .catch(err => {
+    // if there's an error with the server
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+        id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+            model: Category,
+            attributes: ['category_name']
+        },
+        {
+            model: Tag,
+            attributes: ['tag_name']
+        }
+    ]
+})
+.then(dbProduct => {
+    if (!dbProduct) {
+      // if page not found
+        res.status(404).json({ message: 'Unable to find a product with this id' });
+        return;
+    }
+    res.json(dbProduct);
+})
+.catch(err => {
+    console.log(err);
+     // if there's an error with the server
+    res.status(500).json(err);
+});
 });
 
-// create new product
+// create new product with post request
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
-    .then((product) => {
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: [
+      {
+        include: [
+          {
+            model: Tag,
+            through: ProductTag,
+            where: {
+              tag_id: req.body.tag_id
+            }
+          },
+        ],
+      },
+    ],
+  }).then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -47,7 +104,7 @@ router.post('/', (req, res) => {
     });
 });
 
-// update product
+// update product by its id
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -90,7 +147,24 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProduct => {
+    if (!dbProduct) {
+      // if page not found
+        res.status(404).json({ message: 'Unable to find this product' });
+        return;
+    }
+    res.json(dbProduct);
+  })
+  .catch(err => {
+    console.log(err);
+    // if there's an error with the server
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
